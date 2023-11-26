ee87c4353248d3f200b5ccc0ef9293eaa7efab91
JossueE <110933714+JossueE@users.noreply.github.com>
2023-11-25 20:37:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,34 +4,61 @@
 
 using namespace std;
 
-#define NUM_CUARTOS 20
-
-int main(){
+int main() {
+    unsigned int inicio, fin;
     Catalogo    miCatalogo;
-    Dungeon   miDungeon;
+    Dungeon     miDungeon;
 
-    if(!miCatalogo.loadFromCSV("monsters.csv")){
-        cout << "No se puede crear catalogo"<< endl;
+    if(!miCatalogo.loadFromCSV("monsters.csv")) {
+        cout << "No se pudo crear el catalogo" << endl;
+        return 0;
     }
 
     cout << "Creando Dungeon..." << endl;
-    for(int c = 0; c < NUM_CUARTOS; c++){
+    if(!miDungeon.createDungeon("dungeon.txt")) {
+        cout << "No se pudo crear el Dungeon" << endl;
+        return 0;
+    }
+
+    for(unsigned int c = 0; c < miDungeon.getSize(); c ++) {
         Monstruo *pMonster = nullptr, copiaMonstruo;
 
         pMonster = miCatalogo.getRandomMonster();
-        if(!pMonster){
-            cout << "No se pudo cargar el monstruo del catalogo" << endl;
+        if(!pMonster) {
+            cout << "No se pudo obtener un monstruo del catalogo" << endl;
             return 0;
         }
 
         copiaMonstruo = *pMonster;
-        if(!miDungeon.createRoom(copiaMonstruo)){
-            cout << "No se puede insertar cuarto con monstruo al calabozo" << endl;
+        if(!miDungeon.createRoom(c, copiaMonstruo)) {
+            cout << "No se pudo insertar cuarto con monstruo al calabozo" << endl;
             return 0;
         }
     }
 
     miDungeon.imprimeCuartos();
+
+    cout << "Elije una ruta:\n";
+    do {
+        cout << "Dame el cuarto inicial: ";
+        cin >> inicio;
+        cout << "Dame el cuarto final: ";
+        cin >> fin;
+    } while (fin > miDungeon.getSize() || inicio == fin);
+
+    if(!miDungeon.trazaRuta(inicio, fin)) {
+        cout << "No existe ruta de " << inicio << " a " << fin << "\n";
+        return 0;
+    }
+
+    do {
+        Cuarto *actual = nullptr;
+
+        actual = miDungeon.cuartoActualRuta();
+        if(actual != nullptr) {
+            cout << "Entrando a " << *actual << "\n";
+        }
+    } while (miDungeon.avanzaRuta());
 
     return 0;
 }
